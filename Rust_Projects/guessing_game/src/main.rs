4ee38432573259d4e88f4c908bb8e023661a3db7
kelvin mwaijega <130301357+ministerko@users.noreply.github.com>
2024-08-10 01:32:22 +0300

    feat: Added rand dependency and updated main.rs to generate a random secret number

--- Rust_Projects/guessing_game/src/main.rs
+++ Rust_Projects/guessing_game/src/main.rs
@@ -1,14 +1,26 @@
-use std::io;
+//
+//use std::io;
+//
+//fn main(){
+//    println!("Guess the Number!");
+//
+//    println!("input your guess");
+//
+//    let mut guess = String::new();
+//
+//    io::stdin().read_line(&mut guess)
+//       .expect("failed to read the line");
+//    println!("You guessed: {}",guess);
+//
+//}
 
-fn main(){
-    println!("Guess the Number!");
+//using rand 
 
-    println!("input your guess");
+extern crate rand;
 
-    let mut guess = String::new();
+use rand::Rng;
 
-    io::stdin().read_line(&mut guess)
-       .expect("failed to read the line");
-    println!("You guessed: {}",guess);
-
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..=100); // Note the new syntax
+    println!("The secret number is: {}", secret_number);
 }
