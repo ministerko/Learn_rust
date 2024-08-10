b4eb8571517666e69dc183cb5f3b845d45deb12d
kelvin mwaijega <130301357+ministerko@users.noreply.github.com>
2024-08-10 23:04:26 +0300

    Removed commented out code, reorganized main function to include secret number generation and user input

--- Rust_Projects/guessing_game/src/main.rs
+++ Rust_Projects/guessing_game/src/main.rs
@@ -1,26 +1,24 @@
-//
-//use std::io;
-//
-//fn main(){
-//    println!("Guess the Number!");
-//
-//    println!("input your guess");
-//
-//    let mut guess = String::new();
-//
-//    io::stdin().read_line(&mut guess)
-//       .expect("failed to read the line");
-//    println!("You guessed: {}",guess);
-//
-//}
-
-//using rand 
-
-extern crate rand;
-
+use std::io;
 use rand::Rng;
 
-fn main() {
-    let secret_number = rand::thread_rng().gen_range(1..=100); // Note the new syntax
-    println!("The secret number is: {}", secret_number);
+
+
+
+fn main(){
+    println!("Guess the Number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    println!("input your guess");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+       .expect("failed to read the line");
+    println!("You guessed: {guess}");
+   
+
 }
+
