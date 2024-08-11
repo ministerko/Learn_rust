687c15a658f80ecf02e329ba2771146ce7296fe6
kelvin mwaijega <130301357+ministerko@users.noreply.github.com>
2024-08-11 14:57:21 +0300

    Added comment explaining mutability and fixed syntax error in variable declaration

--- Rust_Concepts/variables/src/main.rs
+++ Rust_Concepts/variables/src/main.rs
@@ -1,5 +1,16 @@
+/* 
+But mutability can be very useful, and can make code
+more convenient to write. Although variables are
+immutable by default, you can make them mutable by 
+adding mut in front of the variable name as you did
+in Chapter 2. Adding mut also conveys intent to
+future readers of the code by indicating that othe
+r parts of the code will be changing this variable’s
+value.
+
+*/
 fn main() {
-    let mut let x = 5;
+    let mut x = 5;
 
     println!("the value of x is: {x}");
 
