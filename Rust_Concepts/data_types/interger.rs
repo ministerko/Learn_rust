e5f0554700959803c4d3258eb14714d217df6996
kelvin mwaijega <130301357+ministerko@users.noreply.github.com>
2024-08-11 23:41:15 +0300

    Added: code to parse and print two integer values in main function on interger.rs

---
+++ Rust_Concepts/data_types/interger.rs
@@ -0,0 +1,10 @@
+fn main(){
+
+  let one: u32 = "430".parse().expect("Not a number!");
+
+  let two: u64 = "5900".parse().expect("Not a number! ");
+
+  println!("here the number {one}");
+
+  println!("here the number {two}");
+}
